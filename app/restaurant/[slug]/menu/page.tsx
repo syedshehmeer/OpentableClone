15cd8537b6d524da0bf8b1ca5ffe39044cdbe5b7
Syed Shehmeer Ashfaq <syedshehmeerashfaq@gmail.com>
2023-09-17 06:28:30 +0500

    second commit

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -5,20 +5,26 @@ import RestaurantNavBar from "../components/RestaurantNavBar";
 const prisma = new PrismaClient();
 
 const fetchRestaurantMenu = async (slug: string) => {
-  const restaurant = await prisma.restaurant.findUnique({
-    where: {
-      slug,
-    },
-    select: {
-      items: true,
-    },
-  });
+  try {
+    const restaurant = await prisma.restaurant.findUnique({
+      where: {
+        slug,
+      },
+      select: {
+        items: true,
+      },
+    });
 
-  if (!restaurant) {
-    throw new Error();
+    if (!restaurant) {
+      throw new Error(`Restaurant with slug '${slug}' not found`);
+    }
+
+    return restaurant.items;
+  } catch (error) {
+    // Handle the error gracefully, e.g., log it or display an error message.
+    console.error("Error fetching restaurant menu:", error);
+    throw error; // Rethrow the error to propagate it to the component.
   }
-
-  return restaurant.items;
 };
 
 export default async function RestaurantMenu({
@@ -26,14 +32,21 @@ export default async function RestaurantMenu({
 }: {
   params: { slug: string };
 }) {
-  const menu = await fetchRestaurantMenu(params.slug);
+  try {
+    const menu = await fetchRestaurantMenu(params.slug);
 
-  return (
-    <>
-      <div className="bg-white w-[100%] rounded p-3 shadow">
-        <RestaurantNavBar slug={params.slug} />
-        <Menu menu={menu} />
-      </div>
-    </>
-  );
+    return (
+      <>
+        <div className="bg-white w-[100%] rounded p-3 shadow">
+          <RestaurantNavBar slug={params.slug} />
+          <Menu menu={menu} />
+        </div>
+      </>
+    );
+  } catch (error) {
+    // Handle the error gracefully, e.g., display an error page or message.
+    console.error("Error rendering restaurant menu page:", error);
+    // You can render an error message or a custom error page here.
+    return <div>Error: Unable to fetch restaurant menu.</div>;
+  }
 }
